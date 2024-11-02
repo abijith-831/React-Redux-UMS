75a07c39864a9681b107b50993025f18989f1090
abijith-831 <150845709+abijith-831@users.noreply.github.com>
2024-11-02 16:26:46 +0530

    Update env.js
    l

--- client/src/utility/env.js
+++ client/src/utility/env.js
@@ -1,3 +1,3 @@
-export const BACKENDENDPOINT= import.meta.env.VITE_BACKENDENDPOINT
+export const BACKENDENDPOINT= 'https://react-redux-ums.onrender.com'
 // export const ADMIN_EMAIL= import.meta.env.VITE_ADMIN_EMAIL
 // export const ADMIN_PASSWORD= import.meta.env.VITE_ADMIN_PASSWORD
